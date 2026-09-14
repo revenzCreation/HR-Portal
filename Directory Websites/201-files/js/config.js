83977ea4338d6e33d5bf00983cff82d52b9201cc
Reven Vinzon <revenvinzon011@gmail.com>
2026-09-14 13:52:12 +0800

    commit

--- Directory Websites/201-files/js/config.js
+++ Directory Websites/201-files/js/config.js
@@ -1,1 +1,1 @@
-window.HR_PORTAL_API_URL = 'https://script.google.com/macros/s/AKfycbzMtnoAr3Y8Y8JNsWcUw_Lyv1-lEU_9-QlVExhpqBhHUjseiUz97wyxD6fCnvsRSmrT/exec';
+window.HR_PORTAL_API_URL = 'https://script.google.com/macros/s/AKfycbwUqPvhkgiMg918ap3LbHUkZho9PVubf0n6k9-lF8VbE3-Jhq7W0Wo633KKQyGuK66Bmw/exec';
